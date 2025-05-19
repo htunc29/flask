321fce11affbc24a496bfbd083712f659d4c536d
Hüseyin Tunç <70409978+htunc29@users.noreply.github.com>
2025-05-19 12:52:57 +0300

    Update app.py

--- final_hafta/app.py
+++ final_hafta/app.py
@@ -151,12 +151,12 @@ def gunluk_sil(gunluk_id):
 
 
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    with app.app_context():
+#        db.create_all()
+#    app.run(debug=True)
 
 
-#import os
-#if __name__ == "__main__":
-#    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+import os
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
